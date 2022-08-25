6895d25dbb2d6643f40ffd9c3647775f303ba0c1
Zlata Guseva <gusevazlata96@gmail.com>
2022-08-25 15:31:48 +0400

    Create project, add AutoLayout

---
+++ Calculator/Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Calculator
+//
+//  Created by Злата Гусева on 25.08.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
